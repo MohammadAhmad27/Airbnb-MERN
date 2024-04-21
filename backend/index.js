5ea2b620c41031ba21b73246f770fe8003b93648
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-04-21 15:33:36 +0500

    Updated sample listing code in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
-if (process.env.NODE_ENV != "production") {
-    require('dotenv').config()
-}
+require('dotenv').config()
+
 
 const express = require('express')
 const mongoose = require("mongoose");
@@ -32,13 +31,13 @@ app.get("/", async (req, res) => {
 
 app.post("/", upload.single("listing[image]"), async (req, res, next) => {
     try {
-            let url = req.file.path;
-            let filename = req.file.filename;
-            const newListing = new Listing(req.body.listing);
-            newListing.image = { url, filename }
-            let savedListing = await newListing.save();
-            console.log(savedListing);
-        
+        let url = req.file.path;
+        let filename = req.file.filename;
+        const newListing = new Listing(req.body.listing);
+        newListing.image = { url, filename }
+        let savedListing = await newListing.save();
+        console.log(savedListing);
+
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: "Sorry, Internal server error!" });
@@ -57,22 +56,26 @@ app.post("/", upload.single("listing[image]"), async (req, res, next) => {
 
 
 
-//Home Get testListing Route
+//Code to save testing listing in MongoDB 
 // app.get("/testListing", async (req, res) => {
 //     let sampleListing = new Listing({
-//         title: "My New Villa",
-//         description: "By the beach",
-//         price: 1200,
-//         location: "Calangute, Goa",
-//         country: "India",
+//         title: "Cozy Beachfront Cottage",
+//         description: "Escape to this charming beachfront cottage for a relaxing getaway. Enjoy stunning ocean views and easy access to the beach.",
+//         image: {
+//             filename: "listingimage",
+//             url: "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+//         },
+//         price: 3000,
+//         location: "Malibu",
+//         country: "United States",
 //     });
 //     await sampleListing.save()
-//     .then(()=> {
-//         console.log("Sample saved into DB");
-//     })
-//     .catch((error)=> {
-//         console.log(error.message);
-//     });
+//         .then(() => {
+//             console.log("Sample saved into DB");
+//         })
+//         .catch((error) => {
+//             console.log(error.message);
+//         });
 //     res.send("Successful testing");
 // });
 

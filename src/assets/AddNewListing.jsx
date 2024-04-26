2cf8a26ea646086b9bc861081dc3e1ae1daaaa0d
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-04-26 16:27:35 +0500

    Modified to start work on it

--- src/assets/AddNewListing.jsx
+++ src/assets/AddNewListing.jsx
@@ -2,38 +2,8 @@ import React from 'react'
 
 export default function AddNewListing() {
   return (
-    <div>
-      <h1>Bye</h1>
-      <h1>Bye</h1>
-      <h1>Bye</h1>
-      <h1>Bye</h1>
-      <h1>Bye</h1>
-      <h1>Bye</h1>
-      <h1>Bye</h1>
-      <h1>Bye</h1>
-      <h1>Bye</h1>
-      <h1>Bye</h1>
-      <h1>Bye</h1>
-      <h1>Bye</h1>
-      <h1>Bye</h1>
-      <h1>Bye</h1>
-      <h1>Bye</h1>
-      <h1>Bye</h1>
-      <h1>Bye</h1>
-      <h1>Bye</h1>
-      <h1>Bye</h1>
-      <h1>Bye</h1>
-      <h1>Bye</h1>
-      <h1>Bye</h1>
-      <h1>Bye</h1>
-      <h1>Bye</h1>
-
-
-
-
-
-
-
-    </div>
+    <>
+    
+    </>
   )
 }

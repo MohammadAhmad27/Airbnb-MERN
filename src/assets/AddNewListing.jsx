1c9e3ebe5761da9906e74d2bcb9a733088500f37
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-05-05 15:52:11 +0500

    Changing Form

--- src/assets/AddNewListing.jsx
+++ src/assets/AddNewListing.jsx
@@ -1,30 +1,30 @@
+import e from 'cors'
 import React from 'react'
 
 export default function AddNewListing() {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
     return (
         <>
             <div className="row">
                 <div className="col-8 offset-2">
                     <br /> <br />
                     <h2 className="mb-3">Create a new Lisiting</h2>
-                    <form>
+                    <form onSubmit={handleSubmit}>
 
                         <div className="mb-3">
                             <label htmlFor="title" className="form-label">Title</label>
-                            <input name="listing[title]" placeholder="Add a catchy title" type="text" className="form-control"
+                            <input name="title" placeholder="Add a catchy title" type="text" className="form-control"
                                 required />
-                            <div className="invalid-feedback">
-                                Please enter title!
-                            </div>
                         </div>
 
                         <div className="mb-3">
                             <label htmlFor="description" className="form-label">Description</label>
                             <textarea cols="30" rows="10" name="listing[description]" placeholder="Enter your description"
                                 type="text" className="form-control" required></textarea>
-                            <div className="invalid-feedback">
-                                Please enter short description!
-                            </div>
                         </div>
                         <div className="mb-3">
                             <label htmlFor="image" className="form-label">Image Link</label>
@@ -39,26 +39,17 @@ export default function AddNewListing() {
                             <div className="mb-3 col-md-4">
                                 <label htmlFor="price" className="form-label">Price</label>
                                 <input type="number" name="listing[price]" placeholder="500" className="form-control" required />
-                                <div className="invalid-feedback">
-                                    Price should be valid!
-                                </div>
                             </div>
                             <div className="mb-3 col-md-8">
                                 <label htmlFor="location" className="form-label">Location</label>
                                 <input name="listing[location]" placeholder="Lake Lucerne region" type="text"
                                     className="form-control" required />
-                                <div className="invalid-feedback">
-                                    location should be valid!
-                                </div>
                             </div>
                         </div>
                         <div className="mb-3">
                             <label htmlFor="country" className="form-label">Country</label>
                             <input name="listing[country]" placeholder="Switzerland" type="text" className="form-control"
                                 required />
-                            <div className="invalid-feedback">
-                                Country name should be valid!
-                            </div>
                         </div>
 
                         <button className="btn btn-danger mt-3">Add</button>

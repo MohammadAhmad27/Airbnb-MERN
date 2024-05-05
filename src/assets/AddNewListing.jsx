ded505f954ec90417fccf61f896fedb839a0747f
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-05-05 23:22:43 +0500

    Updated Form content

--- src/assets/AddNewListing.jsx
+++ src/assets/AddNewListing.jsx
@@ -17,39 +17,33 @@ export default function AddNewListing() {
 
                         <div className="mb-3">
                             <label htmlFor="title" className="form-label">Title</label>
-                            <input name="title" placeholder="Add a catchy title" type="text" className="form-control"
-                                required />
+                            <input type="text" id="title" name="title" placeholder="Add a catchy title" className="form-control" minLength={5} required />
                         </div>
-
                         <div className="mb-3">
                             <label htmlFor="description" className="form-label">Description</label>
-                            <textarea cols="30" rows="10" name="listing[description]" placeholder="Enter your description"
-                                type="text" className="form-control" required></textarea>
+                            <textarea type="text" id="description" cols="30" rows="10" name="description" placeholder="Enter your description" className="form-control" minLength={5} required></textarea>
                         </div>
                         <div className="mb-3">
                             <label htmlFor="image" className="form-label">Image Link</label>
-                            <input name="listing[image]" placeholder="Enter image URL/Link" type="text"
-                                className="form-control" />
+                            <input type="text" id="image" name="image" placeholder="Enter image URL/Link" className="form-control" minLength={5} required />
                         </div>
                         {/* <div className="mb-3">
-                    <label htmlFor="image" className="form-label">Uploading Listing Image</label>
-                    <input name="listing[image]" type="file" className="form-control" required />
-                </div> */}
+                            <label htmlFor="image" className="form-label">Uploading Listing Image</label>
+                            <input type="file" id="image" name="image" className="form-control" required />
+                        </div> */}
                         <div className="row">
                             <div className="mb-3 col-md-4">
                                 <label htmlFor="price" className="form-label">Price</label>
-                                <input type="number" name="listing[price]" placeholder="500" className="form-control" required />
+                                <input type="number" id="price" name="price" placeholder="500" className="form-control" minLength={2} required />
                             </div>
                             <div className="mb-3 col-md-8">
                                 <label htmlFor="location" className="form-label">Location</label>
-                                <input name="listing[location]" placeholder="Lake Lucerne region" type="text"
-                                    className="form-control" required />
+                                <input type="text" id="location" name="location" placeholder="Lake Lucerne region" className="form-control" minLength={3} required />
                             </div>
                         </div>
                         <div className="mb-3">
                             <label htmlFor="country" className="form-label">Country</label>
-                            <input name="listing[country]" placeholder="Switzerland" type="text" className="form-control"
-                                required />
+                            <input type="text" id="country" name="country" placeholder="Switzerland" className="form-control" minLength={3} required />
                         </div>
 
                         <button className="btn btn-danger mt-3">Add</button>

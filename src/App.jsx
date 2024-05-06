f8bf82b521154abaa6cb7139d4aaf2db3838bd6e
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-05-06 17:12:14 +0500

    Added AddNewListing.jsx code & Updated App.jsx and Home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -45,28 +45,6 @@ export default App
 
 
 
-// import React, { useState, useEffect } from "react";
-// import "./App.css";
-
-// function App() {
-//   const [message, setMessage] = useState("");
-
-//   useEffect(() => {
-//     fetch("http://localhost:3000/message")
-//       .then((res) => res.json())
-//       .then((data) => setMessage(data.message));
-//   }, []);
-
-//   return (
-//     <div className="App">
-//       <h1>{message}</h1>
-//     </div>
-//   );
-// }
-
-// export default App
-
-
 
 
 
--- src/assets/AddNewListing.jsx
+++ src/assets/AddNewListing.jsx
@@ -1,10 +1,25 @@
 import e from 'cors'
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import listingContext from "../context/ListingContext"
+import { useNavigate } from 'react-router-dom';
 
-export default function AddNewListing() {
+export default function AddNewListing(props) {
+    const context = useContext(listingContext);
+    const { addListing } = context;
+    const [listing, setListing] = useState({ title: "", description: "", image: "", price: "", location: "", country: "" });
+    const navigate = useNavigate();
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        addListing(listing.title, listing.description, listing.image, listing.price, listing.location, listing.country);
+        setListing({ title: "", description: "", image: "", price: "", location: "", country: "" });
+        navigate("/");
+        console.log("Listing Added!")
+        props.showAlert(" Listing Added Successfully!", "success");
+    }
+
+    const onChange = (e) => {
+        setListing({ ...listing, [e.target.name]: e.target.value });
     }
 
     return (
@@ -17,15 +32,15 @@ export default function AddNewListing() {
 
                         <div className="mb-3">
                             <label htmlFor="title" className="form-label">Title</label>
-                            <input type="text" id="title" name="title" placeholder="Add a catchy title" className="form-control" minLength={5} required />
+                            <input type="text" id="title" name="title" value={listing.title} onChange={onChange} placeholder="Add a catchy title" className="form-control" minLength={5} required />
                         </div>
                         <div className="mb-3">
                             <label htmlFor="description" className="form-label">Description</label>
-                            <textarea type="text" id="description" cols="30" rows="10" name="description" placeholder="Enter your description" className="form-control" minLength={5} required></textarea>
+                            <textarea type="text" id="description" cols="30" rows="10" name="description" value={listing.description} onChange={onChange} placeholder="Enter your description" className="form-control" minLength={5} required></textarea>
                         </div>
                         <div className="mb-3">
                             <label htmlFor="image" className="form-label">Image Link</label>
-                            <input type="text" id="image" name="image" placeholder="Enter image URL/Link" className="form-control" minLength={5} required />
+                            <input type="text" id="image" name="image" value={listing.image} onChange={onChange} placeholder="Enter image URL/Link" className="form-control" minLength={5} required />
                         </div>
                         {/* <div className="mb-3">
                             <label htmlFor="image" className="form-label">Uploading Listing Image</label>
@@ -34,16 +49,16 @@ export default function AddNewListing() {
                         <div className="row">
                             <div className="mb-3 col-md-4">
                                 <label htmlFor="price" className="form-label">Price</label>
-                                <input type="number" id="price" name="price" placeholder="500" className="form-control" minLength={2} required />
+                                <input type="number" id="price" name="price" value={listing.price} onChange={onChange} placeholder="500" className="form-control" minLength={2} required />
                             </div>
                             <div className="mb-3 col-md-8">
                                 <label htmlFor="location" className="form-label">Location</label>
-                                <input type="text" id="location" name="location" placeholder="Lake Lucerne region" className="form-control" minLength={3} required />
+                                <input type="text" id="location" name="location" value={listing.location} onChange={onChange} placeholder="Lake Lucerne region" className="form-control" minLength={3} required />
                             </div>
                         </div>
                         <div className="mb-3">
                             <label htmlFor="country" className="form-label">Country</label>
-                            <input type="text" id="country" name="country" placeholder="Switzerland" className="form-control" minLength={3} required />
+                            <input type="text" id="country" name="country" value={listing.country} onChange={onChange} placeholder="Switzerland" className="form-control" minLength={3} required />
                         </div>
 
                         <button className="btn btn-danger mt-3">Add</button>

adb90a0623c390c4c5f4bbf4f0d30c33dae7c502
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-05-07 15:06:47 +0500

    Updated username minlength to 3

--- src/assets/Signup.jsx
+++ src/assets/Signup.jsx
@@ -39,7 +39,7 @@ const Signup = (props) => {
       <form onSubmit={handleSubmit}>
         <div className="my-3">
           <label htmlFor="name" className="form-label">Username</label>
-          <input type="text" className="form-control" id="name" name='name' onChange={onChange} aria-describedby="emailHelp" />
+          <input type="text" className="form-control" id="name" name='name' onChange={onChange} minLength={3} aria-describedby="emailHelp" />
         </div>
         <div className="mb-3">
           <label htmlFor="email" className="form-label">Email address</label>
@@ -48,10 +48,6 @@ const Signup = (props) => {
         <div className="mb-3">
           <label htmlFor="password" className="form-label">Password</label>
           <input type="password" className="form-control" id="password" name='password' onChange={onChange} minLength={3} required />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="cpassword" className="form-label"> Confirm Password</label>
-          <input type="password" className="form-control" id="cpassword" name='cpassword' onChange={onChange} minLength={3} required />
         </div>
 
         <button type="submit" className="btn btn-primary">Submit</button>

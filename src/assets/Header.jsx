75a5776d84d5350810d7caa59f9e0110bbc57635
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-05-07 23:52:00 +0500

    Removed toggle part

--- src/assets/Header.jsx
+++ src/assets/Header.jsx
@@ -74,12 +74,6 @@ export default function Header() {
           <p>Lakes</p>
         </div>
 
-        <div className="tax-toggle">
-          <div className="form-check-reverse form-switch">
-            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Display total after taxes</label>
-          </div>
-        </div>
       </header>
 
 

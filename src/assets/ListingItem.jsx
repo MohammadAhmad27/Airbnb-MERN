cab11dc5fb0dd96ff4b1448a1f3df6b2f407118e
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-05-08 20:44:38 +0500

    Added correct route {`/showlisting/${listing._id}`}

--- src/assets/ListingItem.jsx
+++ src/assets/ListingItem.jsx
@@ -7,14 +7,14 @@ const ListingItem = (props) => {
     return (
 
         <div className="col-lg-4 col-md-6 col-sm-12 mt-4">
-            <Link to={`/listings/${listing._id}`} className="listing-link">
+            <Link to={`/showlisting/${listing._id}`} className="listing-link">
                 <div className="card listing-card">
                     <img src={listing.image} className="card-img-top" alt="listing" style={{ height: "18rem" }} />
                     <div className="card-body">
                         <p className="card-text">
                             <b>{listing.title}</b>
                             <br />
-                            ${listing.price.toLocaleString("en-US")} /night
+                            ${listing.price.toLocaleString("en-US")}/night
                         </p>
                     </div>
                 </div>

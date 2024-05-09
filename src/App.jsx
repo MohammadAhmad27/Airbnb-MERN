65ab51cfe6f97edb5152ea0e8a53f2a0bfe63272
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-05-09 20:15:50 +0500

    Added Edit (Component & Route)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { useState } from 'react'
 import Navbar from './assets/Navbar'
 import Home from './assets/Home'
 import AddNewListing from './assets/AddNewListing'
@@ -7,8 +8,8 @@ import ListingState from './context/ListingState'
 import Alert from './assets/Alert'
 import Login from './assets/Login'
 import Signup from './assets/Signup'
-import { useState } from 'react'
 import ListingPage from './assets/ListingPage'
+import Edit from './assets/Edit'
 
 function App() {
   const [alert, setAlert] = useState(null);
@@ -32,6 +33,7 @@ function App() {
             <Route exact path="/" element={<Home showAlert={showAlert} />} />
             <Route exact path="/addnewlisting" element={<AddNewListing showAlert={showAlert} />} />
             <Route exact path="/showlisting/:id" element={<ListingPage showAlert={showAlert} />} />
+            <Route exact path="/editlisting/:id" element={<Edit showAlert={showAlert} />} />
             <Route exact path="/login" element={<Login showAlert={showAlert} />} />
             <Route exact path="/signup" element={<Signup showAlert={showAlert} />} />
           </Routes>

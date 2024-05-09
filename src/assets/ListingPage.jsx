d6b04fa19cf28c5599e65218d0df029da9aa94df
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-05-09 20:00:29 +0500

    Added authentication mechanism for DELETE & EDIT buttons

--- src/assets/ListingPage.jsx
+++ src/assets/ListingPage.jsx
@@ -27,7 +27,40 @@ const ListingPage = (props) => {
     fetchData();
   }, [id]);
 
-  if (!listing) return <div>Loading...</div>;
+  if (!listing) {
+    return (
+      <div>
+        <h1>Error in finding requested listing!</h1>
+      </div>
+    )
+  }
+
+
+  const isOwner = () => {
+    const token = localStorage.getItem('token');
+    console.log('token', token);
+    if (!token) return false;
+    const decodedToken = parseJwt(token);
+    console.log('compare', decodedToken.user.id === listing.user);
+    console.log(typeof decodedToken.user.id);
+    console.log(typeof listing.user);
+    console.log('user', decodedToken.user.id);
+    console.log('user1', listing.user);
+    return decodedToken?.user.id?.toString() === listing?.user?.toString();
+  };
+
+  const parseJwt = (token) => {
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+  };
+
+
+
 
   return (
     <>
@@ -49,16 +82,20 @@ const ListingPage = (props) => {
 
       <div className='offset-3'>
         <Stack direction="row" spacing={5} >
-          <Button variant="contained" color="error" startIcon={<DeleteIcon />} onClick={() => {
-            deleteListing(listing._id);
-            props.showAlert('Listing Deleted Successfully!', 'success');
-            navigate("/");
-          }}>
-            Delete
-          </Button>
-          <Button variant="contained" color="error" endIcon={<EditIcon />}>
-            Edit
-          </Button>
+          {isOwner() && (
+            <Button variant="contained" color="error" startIcon={<DeleteIcon />} onClick={() => {
+              deleteListing(listing._id);
+              props.showAlert('Listing Deleted Successfully!', 'success');
+              navigate("/");
+            }}>
+              Delete
+            </Button>
+          )}
+          {isOwner() && (
+            <Button variant="contained" color="error" endIcon={<EditIcon />}>
+              Edit
+            </Button>
+          )}
         </Stack>
       </div>
 

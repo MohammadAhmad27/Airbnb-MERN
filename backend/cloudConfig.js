26cc49f6118500a39e81a3760b6882e26b857b4b
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-05-16 01:51:43 +0500

    Added cloudConfig.js

--- backend/cloudConfig.js
+++ backend/cloudConfig.js
@@ -2,17 +2,20 @@ const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET
+  cloud_name: 'dovnqmjna',
+  api_key: '959394489267139',
+  api_secret: 'MwBw7cn-oqrgOWZb0VaKyPC7xjY'
 });
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: 'airbnb_DEV',
-    format: async (req, file) => "png" || "jpg" || "jpeg", // supports promises as well
-    public_id: (req, file) => 'computed-filename-using-request',
+    format: async (req, file) => {
+      const ext = file.mimetype.split('/')[1];
+      return ext === 'png' || ext === 'jpg' || ext === 'jpeg' ? ext : 'jpg';
+    },
+    public_id: (req, file) => `listing_${Date.now()}`,
   },
 });
 

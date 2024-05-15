9caf371fd78283e29a249d8d8e9d271c668db823
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-05-16 01:56:16 +0500

    Updated code for image upload

--- src/assets/Edit.jsx
+++ src/assets/Edit.jsx
@@ -3,8 +3,7 @@ import { useParams, useNavigate } from "react-router-dom";
 import listingContext from "../context/ListingContext";
 
 const Edit = ({ showAlert }) => {
-  const { id } = useParams(); // Get the dynamic parameter from the URL
-  console.log("Listing ID:", id); // Log the id to ensure it's correct
+  const { id } = useParams();
   const context = useContext(listingContext);
   const { showListing, editListing } = context;
   const [listing, setListing] = useState({
@@ -33,14 +32,23 @@ const Edit = ({ showAlert }) => {
         });
       } catch (error) {
         console.error("Error fetching listing:", error);
+        showAlert("Error fetching listing.", "danger");
       }
     };
 
     fetchListing();
-  }, [id, showListing]);
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const formData = new FormData();
+    formData.append("title", listing.etitle);
+    formData.append("description", listing.edescription);
+    if (listing.eimage) formData.append("image", listing.eimage);
+    formData.append("price", listing.eprice);
+    formData.append("location", listing.elocation);
+    formData.append("country", listing.ecountry);
+
     editListing(
       listing.id,
       listing.etitle,
@@ -55,134 +63,130 @@ const Edit = ({ showAlert }) => {
   };
 
   const onChange = (e) => {
-    setListing({ ...listing, [e.target.name]: e.target.value });
+    if (e.target.name === "eimage") {
+      setListing({ ...listing, eimage: e.target.files[0] });
+    } else {
+      setListing({ ...listing, [e.target.name]: e.target.value });
+    }
   };
 
   return (
-    <>
-      <div className="row">
-        <div className="col-8 offset-2">
-          <br /> <br />
-          <h2 className="mb-3">Edit Listing</h2>
-          <form onSubmit={handleSubmit}>
-            <div className="mb-3">
-              <label htmlFor="title" className="form-label">
-                Title
+    <div className="row">
+      <div className="col-8 offset-2">
+        <br /> <br />
+        <h2 className="mb-3">Edit Listing</h2>
+        <form onSubmit={handleSubmit} encType="multipart/form-data">
+          <div className="mb-3">
+            <label htmlFor="title" className="form-label">
+              Title
+            </label>
+            <input
+              type="text"
+              id="title"
+              name="etitle"
+              value={listing.etitle}
+              onChange={onChange}
+              placeholder="Add a catchy title"
+              className="form-control"
+              minLength={5}
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="description" className="form-label">
+              Description
+            </label>
+            <textarea
+              cols="30"
+              rows="10"
+              id="description"
+              name="edescription"
+              value={listing.edescription}
+              onChange={onChange}
+              placeholder="Enter your description"
+              className="form-control"
+              required
+            ></textarea>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="image" className="form-label">
+              Image
+            </label>
+            <input
+              type="file"
+              id="image"
+              name="eimage"
+              onChange={onChange}
+              className="form-control"
+            />
+          </div>
+          <div className="row">
+            <div className="mb-3 col-md-4">
+              <label htmlFor="price" className="form-label">
+                Price
               </label>
               <input
-                type="text"
-                id="title"
-                name="etitle"
-                value={listing.etitle}
+                type="number"
+                id="price"
+                name="eprice"
+                value={listing.eprice}
                 onChange={onChange}
-                placeholder="Add a catchy title"
+                placeholder="500"
                 className="form-control"
-                minLength={5}
+                min={10}
                 required
               />
             </div>
-            <div className="mb-3">
-              <label htmlFor="description" className="form-label">
-                Description
-              </label>
-              <textarea
-                cols="30"
-                rows="10"
-                id="description"
-                name="edescription"
-                value={listing.edescription}
-                onChange={onChange}
-                placeholder="Enter your description"
-                className="form-control"
-                required
-              ></textarea>
-            </div>
-            <div className="mb-3">
-              <label htmlFor="image" className="form-label">
-                Image Link
+            <div className="mb-3 col-md-8">
+              <label htmlFor="location" className="form-label">
+                Location
               </label>
               <input
                 type="text"
-                id="image"
-                name="eimage"
-                value={listing.eimage}
+                id="location"
+                name="elocation"
+                value={listing.elocation}
                 onChange={onChange}
-                placeholder="Enter image URL/Link"
-                className="form-control"
-                minLength={15}
-                required
-              />
-            </div>
-            <div className="row">
-              <div className="mb-3 col-md-4">
-                <label htmlFor="price" className="form-label">
-                  Price
-                </label>
-                <input
-                  type="number"
-                  id="price"
-                  name="eprice"
-                  value={listing.eprice}
-                  onChange={onChange}
-                  placeholder="500"
-                  className="form-control"
-                  min={10}
-                  required
-                />
-              </div>
-              <div className="mb-3 col-md-8">
-                <label htmlFor="location" className="form-label">
-                  Location
-                </label>
-                <input
-                  type="text"
-                  id="location"
-                  name="elocation"
-                  value={listing.elocation}
-                  onChange={onChange}
-                  placeholder="Lake Lucerne region"
-                  className="form-control"
-                  minLength={3}
-                  required
-                />
-              </div>
-            </div>
-            <div className="mb-3">
-              <label htmlFor="country" className="form-label">
-                Country
-              </label>
-              <input
-                type="text"
-                id="country"
-                name="ecountry"
-                value={listing.ecountry}
-                onChange={onChange}
-                placeholder="Switzerland"
+                placeholder="Lake Lucerne region"
                 className="form-control"
                 minLength={3}
                 required
               />
             </div>
-
-            <button
-              disabled={
-                listing.etitle.length < 5 ||
-                listing.edescription.length < 5 ||
-                listing.eimage.length < 15 ||
-                listing.eprice.length < 2 ||
-                listing.elocation.length < 3 ||
-                listing.ecountry.length < 3
-              }
-              className="btn btn-danger mt-3"
-            >
-              Add
-            </button>
-            <br />
-            <br />
-          </form>
-        </div>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="country" className="form-label">
+              Country
+            </label>
+            <input
+              type="text"
+              id="country"
+              name="ecountry"
+              value={listing.ecountry}
+              onChange={onChange}
+              placeholder="Switzerland"
+              className="form-control"
+              minLength={3}
+              required
+            />
+          </div>
+          <button
+            disabled={
+              listing.etitle.length < 5 ||
+              listing.edescription.length < 5 ||
+              listing.eprice.length < 2 ||
+              listing.elocation.length < 3 ||
+              listing.ecountry.length < 3
+            }
+            className="btn btn-danger mt-3"
+          >
+            Save Changes
+          </button>
+          <br />
+          <br />
+        </form>
       </div>
-    </>
+    </div>
   );
 };
 
